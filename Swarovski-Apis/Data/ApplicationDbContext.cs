40c4bd5b1786f997e42f4ad1a607a1bb94de7812
Tabby <tluvuno@seaburysolutions.com>
2024-07-03 10:58:37 +0300

    Create database

---
+++ Swarovski-Apis/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Swarovski_Apis.Data
+{
+    //DbContext is a base class inherited from entity framowork core that is allows one 
+    //to connect to database and allows you to query and save instances of entities
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
