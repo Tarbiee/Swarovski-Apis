e36f973d324ef3a57fb48bc4c55b87ee545fba12
Tabby <tluvuno@seaburysolutions.com>
2024-08-06 12:17:31 +0300

    method to register urls

--- Swarovski-Apis/Controllers/MpesaPaymentController.cs
+++ Swarovski-Apis/Controllers/MpesaPaymentController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using MpesaSdk;
 using MpesaSdk.Interfaces;
+using Newtonsoft.Json;
+using Swarovski_Apis.Models;
+using System.Text;
 
 namespace Swarovski_Apis.Controllers
 {
@@ -33,12 +36,50 @@ namespace Swarovski_Apis.Controllers
             var response = await client.SendAsync(request);
             var mpesaResponse = await response.Content.ReadAsStringAsync(); 
 
+            TokenResponseDto tokenObject = JsonConvert.DeserializeObject<TokenResponseDto>(mpesaResponse);
+            
+            return tokenObject.access_token;
+        }
+        // Register URL
+        [HttpGet("register-urls")]
+        public async Task<string> RegisterMpesaUrls()
+        {
+            // Prepare the JSON body for registration
+            var jsonBody = JsonConvert.SerializeObject(new
+            {
+                ValidationURL = "https://mydemo-url.com/validation",
+                ConfirmationURL = "https://mydemo-url.com/confirmation",
+                ResponseType = "Completed",
+                ShortCode = "600986"
+            });
 
+            var jsonReadyBody = new StringContent
+            (
+                jsonBody,
+                Encoding.UTF8,
+                "application/json"
+            );
 
-            return mpesaResponse;
+            // Get the access token
+            var token = await GetToken();
+
+            var client = _clientFactory.CreateClient("mpesa");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}"); // Add space after Bearer
+
+            var url = "/mpesa/c2b/v1/registerurl";
+
+            // Send POST request to register URL
+            var response = await client.PostAsync(url, jsonReadyBody);
+
+            var registerResponse = await response.Content.ReadAsStringAsync();
+
+            // Log the response for debugging
+            Console.WriteLine("Register Response: " + registerResponse);
+
+            return registerResponse;
         }
 
-        
+
 
     }
 }
--- Swarovski-Apis/Models/TokenResponseDto.cs
+++ Swarovski-Apis/Models/TokenResponseDto.cs
@@ -2,6 +2,8 @@
 {
     public class TokenResponseDto
     {
-        
+        public string access_token { get; set; }
+        public string expires_in { get; set; }
+
     }
 }
